ab27616eb6c2187d8b2a92220d27d73498337d17
Kakarot <105483542+caRot204@users.noreply.github.com>
2023-11-17 18:00:38 +0700

    update 4

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -31,7 +31,7 @@ const ProfilePage = () => {
                     </svg>
                 </div>
             </section>
-            <section className="relative py-16 bg-blueGray-200">
+            <section className="relative py-12 bg-blueGray-200">
                 <div className="container mx-auto px-4">
                     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
                         <div className="px-6">
@@ -47,7 +47,7 @@ const ProfilePage = () => {
                                 </div>
                                 <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                                     <div className="py-6 px-3 mt-32 sm:mt-0">
-                                        <a href="https://www.facebook.com/bnoway169"><button
+                                        <a href="#"><button
                                             className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                                             type="button"
                                         >
@@ -95,13 +95,13 @@ const ProfilePage = () => {
                                     FPT Polytechnic
                                 </div>
                             </div>
-                            <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+                            <div className="mt-5 py-10 border-t border-blueGray-200 text-center">
                                 <div className="flex flex-wrap justify-center">
                                     <div className="w-full lg:w-9/12 px-4">
                                         <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                                            Github
+                                            Xin chào !
                                         </p>
-                                        <a href="https://github.com/caRot204" className="font-normal text-pink-500">
+                                        <a href="#" className="font-normal text-pink-500">
                                             Github
                                         </a>
                                     </div>
